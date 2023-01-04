a26d34e1daab086dcdc60b7665aa50f114e3f666
BrendanDenzel <77647191+BrendanDenzel@users.noreply.github.com>
2023-01-04 13:01:45 -0500

    Add files via upload

---
+++ bookmarklet.js
@@ -0,0 +1,1 @@
+javascript:(function () {var a = document.createElement('script');a.src = 'https://cdn.jsdelivr.net/gh/BrendanDenzel/test-ingot@latest/ingot.min.js';document.body.appendChild(a);}())
